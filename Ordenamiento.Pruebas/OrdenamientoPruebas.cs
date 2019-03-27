5e575c813c77c95c46dd68c33d6ff10890280e97
Usuario <rflores9@ucol.mx>
2019-03-27 12:02:10 -0600

    agregar el proyecto de pruebas

--- Ordenamiento.Pruebas/OrdenamientoPruebas.cs
+++ Ordenamiento.Pruebas/OrdenamientoPruebas.cs
@@ -2,33 +2,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Ordenamiento.Pruebas
 {
-    [TestMethod]
+    [TestClass]
     public class PruebasOrdenamiento
     {
         private int[] salidaEsperada = new int[] {1,2,3,4,5};
         private Ordenamiento.Burbuja ordenar = new Ordenamiento.Burbuja();
         [TestMethod]
-        public void PruebaOrdenado(){
+        public void PruebaOrdenado() {
             int[] entrada = new int[]{1,2,3,4,5};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada, entrada);
         }
-         public void PruebaOrdenado(){
+        [TestMethod]
+         public void PruebaMixed(){
             int[] entrada = new int[]{1,3,2,4,5};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada, entrada);
         }
+        [TestMethod]
         public void Pruebafinal(){
             int[] entrada = new int[]{5,1,2,3,4};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada, entrada);
         }
-
+        [TestMethod]
         public void PruebaSwap(){
             int[] entrada = new int[]{2,3,4,5,1};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada, entrada);
         }
+        [TestMethod]
         public void PruebaInvertido(){
             int[] entrada = new int[]{5,4,3,2,1};
             ordenar.Ordenar(entrada);
